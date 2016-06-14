d578b398447ec22ae7f30939d7c1b8ae91b65943
Tim Vrakas <tim.vrakas@gmail.com>
2016-06-13 22:03:57 -0500

    Kek

--- Reset/Reset.ino
+++ Reset/Reset.ino
@@ -1,13 +1,16 @@
-int greenLED = 5;
+int greenLED = 12;
 int redLED = 6;
 
 void setup() {
   Serial1.begin(4800);
- //Cold Reset
- //Serial1.write("$PSRF101,0,0,0,0,0,0,12,4*10\r\n");
- 
- //Factory Reset
- //Serial1.write("$PSRF101,0,0,0,0,0,0,12,8*1C\r\n");
+  delay(2000);
+
+  //Factory Reset
+  //Serial1.write("$PSRF101,0,0,0,0,0,0,12,8*1C\r\n");
+
+  //Cold Reset
+  //Serial1.write("$PSRF101,0,0,0,0,0,0,12,4*10\r\n");
+
   pinMode(greenLED, OUTPUT);
   pinMode(redLED, OUTPUT);
 }
